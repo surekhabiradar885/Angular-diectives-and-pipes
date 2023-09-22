0417569f04354a2f77520e98be0f8bef130ab73b
unknown <biradarsurekha@gmail.com>
2023-09-22 14:46:21 +0100

    directive added

---
+++ src/app/if.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, TemplateRef, ViewContainerRef, Input } from '@angular/core';
+@Directive({
+  selector: '[appIf]',
+})
+export class IfDirective {
+  private hasView = false;
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) {}
+  @Input() set appIf(condition: boolean) {
+    if (condition && !this.hasView) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.hasView = true;
+    } else if (!condition && this.hasView) {
+      this.viewContainer.clear();
+      this.hasView = false;
+    }
+  }
+}
