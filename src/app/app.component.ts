e0d398cab6279bd7a5eb6694350e6e2079fda8c4
unknown <biradarsurekha@gmail.com>
2023-09-22 18:55:22 +0100

    custom pipes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   title = 'custom-directives-and-pipes';
   color = '';
   condition: boolean = true;
+
+  //pipes
+  name: Promise<string | null>;
+  value: number = 0;
+
+  constructor() {
+    this.name = this.getName();
+  }
+
+  getName(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      setInterval(() => {
+        resolve('John Mack');
+      }, 5000);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { IfDirective } from './if.directive';
+import { SqrtPipe } from './sqrt.pipe';
 
 @NgModule({
-  declarations: [AppComponent, HighlightDirective, IfDirective],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HighlightDirective, IfDirective, SqrtPipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
